0209b4cb572646737805f42a17d422183b86f228
yanger616@gmail.com <jihyan@microsoft.com>
2025-03-07 18:04:11 +0800

    Add first official backend api version

--- KnowledgeCreationTimeBackend/KnowledgeCreationTimeBackend/Controllers/KCTBackendController.cs
+++ KnowledgeCreationTimeBackend/KnowledgeCreationTimeBackend/Controllers/KCTBackendController.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.Configuration;
 using System.ClientModel;
 using KnowledgeCreationTimeBackend;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using OpenAI;
+using OpenAI.Chat;
 
 namespace KnowledgeCreationTimeBackend.Controllers
 {
@@ -14,11 +16,13 @@ namespace KnowledgeCreationTimeBackend.Controllers
     {
         private readonly string _endpointBase;
         private readonly string _apiKey;
+        private readonly AzureOpenAIClient _azureClient;
 
         public KCTBackendController(IConfiguration configuration)
         {
             _endpointBase = configuration["AZURE_OPENAI_ENDPOINT"] ?? throw new ArgumentNullException(nameof(configuration), "AZURE_OPENAI_ENDPOINT is not configured.");
             _apiKey = configuration["AZURE_OPENAI_KEY"] ?? throw new ArgumentNullException(nameof(configuration), "AZURE_OPENAI_KEY is not configured.");
+            _azureClient = new AzureOpenAIClient(new Uri(_endpointBase), new ApiKeyCredential(_apiKey));
         }
 
         [HttpPost]
@@ -32,17 +36,29 @@ namespace KnowledgeCreationTimeBackend.Controllers
                 result.Content = "Type and content are required.";
             }
             try
-            {
-                var azureClient = new AzureOpenAIClient(new Uri(_endpointBase), new ApiKeyCredential(_apiKey));
+            {                
                 if(type == "Text")
                 {
-                    var chatClient = azureClient.GetChatClient("gpt-4o");
+                    var chatClient = _azureClient.GetChatClient("gpt-4o");
                     var response = await chatClient.CompleteChatAsync(content);
                     result.Content = response.Value.Content[0].Text;
                 }
-                else if (type == "Image")
+                else if (type == "ImageToText")
                 {
-                    var imageClient = azureClient.GetImageClient("gpt-4o");
+                    var chatContent = ChatMessageContentPart.CreateImagePart(new Uri(content));
+                    var messages = new ChatMessage[]
+                    {
+                        new UserChatMessage(chatContent),
+                        new SystemChatMessage("你是一个专业的图片分析师，请分析链接中图片的内容并且给出最终的场景推测，不要显示分析过程。")
+                    };
+                    var chatClient = _azureClient.GetChatClient("gpt-4o");
+                    
+                    var response = await chatClient.CompleteChatAsync(messages);
+                    result.Content = response.Value.Content[0].Text;
+                }
+                else if (type == "TextToImage")
+                {
+                    var imageClient = _azureClient.GetImageClient("dall-e-3");
                     var response = await imageClient.GenerateImageAsync(content);
                     result.Content = response.Value.ImageUri.AbsoluteUri;
                 }
@@ -53,6 +69,15 @@ namespace KnowledgeCreationTimeBackend.Controllers
             {
                 result.Content = ex.Message;
                 return result;
+            }
+        }
+
+        private async Task<string> ImageURLToBase64(string imageUrl)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var imageBytes = await httpClient.GetByteArrayAsync(imageUrl);
+                return Convert.ToBase64String(imageBytes);
             }
         }
     }
---
+++ KnowledgeCreationTimeBackend/KnowledgeCreationTimeBackend/Controllers/UploadController.cs
@@ -0,0 +1,84 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KnowledgeCreationTimeBackend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        private readonly string _connectionString;
+
+        public UploadController(IConfiguration configuration)
+        {
+            _connectionString = configuration["ConnectionString"] ?? throw new ArgumentNullException(nameof(configuration), "ConnectionString is not configured.");
+        }
+
+        // POST Upload
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile formFile)
+        {
+            try
+            {
+                if (formFile == null || string.IsNullOrEmpty(formFile.FileName))
+                {
+                    var requestFiles = HttpContext?.Request?.Form?.Files;
+                    if (requestFiles == null || requestFiles.Count == 0)
+                        return BadRequest("No file uploaded.");
+
+                    var file = requestFiles[0];
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "uploads");
+                    if (!Directory.Exists(filePath))
+                    {
+                        Directory.CreateDirectory(filePath);
+                    }
+
+                    var newFileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    using (var stream = new MemoryStream())
+                    {
+                        await file.CopyToAsync(stream);
+                        var url = await UploadFileAsBlob(stream, newFileName);
+                        return Ok(url);
+                    }
+                }
+                else
+                {
+                    var newFileName = Guid.NewGuid().ToString() + Path.GetExtension(formFile.FileName);
+                    using (var stream = new MemoryStream())
+                    {
+                        await formFile.CopyToAsync(stream, CancellationToken.None);
+                        var url = await UploadFileAsBlob(stream, newFileName);
+                        return Ok(url);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        private async Task<string> UploadFileAsBlob(Stream stream, string filename)
+        {
+            try
+            {
+                var containerName = "image";
+
+                var blobServiceClient = new BlobServiceClient(_connectionString);
+                var blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
+                var blobClient = blobContainerClient.GetBlobClient(filename);
+
+                stream.Position = 0;
+                await blobClient.UploadAsync(stream, true);
+
+                return blobClient.Uri.ToString();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
